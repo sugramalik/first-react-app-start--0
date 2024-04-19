2620812ca3fc04764dcf6ab6f61eb34dafed04f8
sughra malik <sugraaaaa@gmail.com>
2024-04-19 08:21:28 +0000

    Adding one more tab element

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ const content = [
     "React is extremely popular",
     "It makes building complex, interactive UIs a breeze",
     "It's powerful & flexible",
-    "It has a very active and versatile ecosystem"
+    "It has a very active and versatile ecosystem",
   ],
   [
     "Components, JSX & Props",
     "State",
     "Hooks (e.g., useEffect())",
-    "Dynamic rendering"
+    "Dynamic rendering",
   ],
   [
     "Official web page (react.dev)",
     "Next.js (Fullstack framework)",
-    "React Native (build native mobile apps with React)"
+    "React Native (build native mobile apps with React)",
   ],
   [
     "Vanilla JavaScript requires imperative programming",
     "Imperative Programming: You define all the steps needed to achieve a result",
     "React on the other hand embraces declarative programming",
-    "With React, you define the goal and React figures out how to get there"
-  ]
+    "With React, you define the goal and React figures out how to get there",
+  ],
 ];
 
 export default function App() {
@@ -59,6 +59,12 @@ export default function App() {
             onClick={() => setActiveContentIndex(2)}
           >
             Related Resources
+          </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            React vs JS
           </button>
         </menu>
         <div id="tab-content">
